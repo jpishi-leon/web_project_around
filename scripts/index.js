7af35578abd086a4ea2242cbaa6158248380a170
jpishi-leon <boas.mousy-0p@icloud.com>
2025-04-19 19:19:00 -0600

    last changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-const openProfilePopupBttn = document.querySelector(".profile__lapiz"); //creacion de las variables
+const openProfilePopupBttn = document.querySelector(
+  "#profile__lapiz-action-add"
+);
 const profileFormPopup = document.querySelector(".popup");
 const closeProfilePopupBttn = document.querySelector(
   ".popup__container-bttn-close"
@@ -6,13 +8,13 @@ const closeProfilePopupBttn = document.querySelector(
 const saveProfilePopupBttn = document.querySelector(
   ".popup__form-input_submit"
 );
-const inputProfileName = document.queryselector("#InputName");
-const inputProfileAbout = document.queryselector("#InputAbout");
-const nameProfileHeader = document.queryselector(".profile__details-name");
-const aboutProfileHeader = document.queryselector(
+const inputProfileName = document.querySelector("#InputName");
+const inputProfileAbout = document.querySelector("#InputAbout");
+const nameProfileHeader = document.querySelector(".profile__details-name");
+const aboutProfileHeader = document.querySelector(
   ".profile__details-profession"
 );
-const formElement = document.queryselector(".popup__form");
+const formElement = document.querySelector(".popup__form");
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
@@ -24,11 +26,12 @@ function handleProfileFormSubmit(evt) {
 openProfilePopupBttn.addEventListener("click", () => {
   inputProfileName.value = nameProfileHeader.textContent;
   inputProfileAbout.value = aboutProfileHeader.textContent;
-  profileFormPopup.classList.add(".popup__show");
-  saveProfilePopupBttn.classList.add(".popup__form-btn-submit-disable");
+  profileFormPopup.classList.add("popup__show");
+  saveProfilePopupBttn.classList.add("popup__form-btn-submit-disable");
 });
 
 closeProfilePopupBttn.addEventListener("click", () => {
-  profileFormPopup.classList.remove(".popup__show");
+  profileFormPopup.classList.remove("popup__show");
 });
+
 formElement.addEventListener("submit", handleProfileFormSubmit);
