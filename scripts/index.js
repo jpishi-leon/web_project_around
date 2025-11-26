487156c76fd02ee96f99ac6f50aa0a0698d97ba5
jpishi-leon <boas.mousy-0p@icloud.com>
2025-11-26 10:22:00 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-import { Card } from "../components/Card.js";
-import { FormValidator } from "../components/FormValidator.js";
-import Section from "../components/Section.js";
-import UserInfo from "../components/UserInfo.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import PopupWithImage from "../components/PopupWithImage.js";
+import { Card } from "./components/Card.js";
+import { FormValidator } from "./components/FormValidator.js";
+import Section from "./components/Section.js";
+import UserInfo from "./components/UserInfo.js";
+import PopupWithForm from "./components/PopupWithForm.js";
+import PopupWithImage from "./components/PopupWithImage.js";
 
 const initialCards = [
   {
